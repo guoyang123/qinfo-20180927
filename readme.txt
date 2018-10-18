43e3ba4236deae2c90200e4223aa9d898630e46d
gy <3137772593@qq.com>
2018-10-18 09:04:19 +0800

    add

--- readme.txt
+++ readme.txt
@@ -2,3 +2,17 @@
 springboot集成mybatis
 https://blog.csdn.net/a67474506/article/details/52013771
 test
+
+
+mvn clean package -Dmaven.test.skip=true
+scp target/qinfo.jar gy@39.105.63.115:/usr/gy/javaapp
+
+
+cd javaapp
+rm -rf qinfo.jar
+ps -ef | grep qinfo
+kill -9 processid
+nohup java -jar qinfo.jar > /dev/null 2>&1 &
+
+访问：
+http://gsmzzk.com:8080/info
--- src/main/java/com/neuedu/controller/front/FrontController.java
+++ src/main/java/com/neuedu/controller/front/FrontController.java
@@ -2,12 +2,17 @@ package com.neuedu.controller.front;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class FrontController {
-@RequestMapping(value = "/info")
- public String  front(){
-     return "front";
- }
+@RequestMapping(value = "/info",method = RequestMethod.GET)
+public String  front(){
+    return "front";
+}
+    @RequestMapping(value = "/info",method = RequestMethod.POST)
+    public String  frontpost(){
+        return "front2";
+    }
 
 }
